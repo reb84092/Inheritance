46f8457289642e82138929713b53b51e49c01abc
reb84092 <reb84092@gmail.com>
2016-03-28 09:53:47 -0600

    Started inheritance project

---
+++ src/inheritance/model/DS.java
@@ -0,0 +1,14 @@
+package inheritance.model;
+
+/**
+ * @author rbar3155
+ */
+
+public abstract class DS
+{
+	
+	//Three Data Members
+	private int girlsAge;
+	private String girlsName;
+	private double girlsBirthdate;
+}
